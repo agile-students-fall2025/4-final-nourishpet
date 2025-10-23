d453206825ad76d96251fbe8f043a5df3805442e
BlackCloud-k <niyujian2004@126.com>
2025-10-23 16:58:00 -0400

    user page

--- front-end/src/pages/UserPage.js
+++ front-end/src/pages/UserPage.js
@@ -1,15 +1,73 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
 function UserPage(){
+    const [userData, setUserData] = useState({
+        name: "Charlie",
+        petName: "Charlie's dog",
+        targetWeight: "50kg",
+        height: "160cm",
+        currentWeight: "55kg",
+        bmi: "21.5 Standard"
+      });
+
     return(
         <div>
-            <div>User Info</div>
+            <div className="user-page">
+                <div className="page-header">
+                    <h1>User Page</h1>
+                    <EditButton />
+                </div>
+            
+            <UserInfoDisplay userData={userData} />
+            
+            <LogoutButton />
+            </div>
+
             <button>
                 <Link to="/">Home Page</Link>
             </button>
         </div>
     )
+}
+
+function UserInfoDisplay( {userData} ){
+    return (
+        <div className="user-info-display">
+
+          <div className="user-portrait">
+            <div className="portrait-placeholder">
+              User Portraits
+            </div>
+          </div>
+          
+          <h2 className="user-name">{userData.name}</h2>
+          
+          <div className="user-details">
+            <p>Pet Name: {userData.petName}</p>
+            <p>Target Weight: {userData.targetWeight}</p>
+            <p>Height: {userData.height}</p>
+            <p>Current Weight: {userData.currentWeight}</p>
+            <p>BMI: {userData.bmi}</p>
+          </div>
+        </div>
+      );
+}
+
+function LogoutButton() {
+    return (
+      <button className="logout-button">
+        <Link to='/log_out'>Log Out</Link>
+      </button>
+    );
+  }
+
+function EditButton() {
+return (
+    <button className="edit-button">
+        <Link to='edit_user_info'>Edit</Link>
+    </button>
+);
 }
 
 export default UserPage;
