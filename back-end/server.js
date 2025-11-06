364241ea421a01a742663076110441731868ebfa
Tonyzsp <sz3822@nyu.edu>
2025-11-06 18:20:04 -0500

    change to local json file

--- back-end/server.js
+++ back-end/server.js
@@ -1,15 +1,21 @@
 import express from "express";
-import axios from "axios";
 import cors from "cors";
 import { readFileSync, writeFileSync } from "fs";
+import path from 'path'
+import { fileURLToPath } from 'url'
+import morgan from "morgan";
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express();
-app.use(cors()); // allow React frontend to access backend
+app.use(morgan("dev"));
+app.use(cors()); 
 app.use(express.json());
 
 app.get("/api/petdata", async (req, res) => {
   try {
-    const rawData = readFileSync("./temp_data/todayData.json", "utf-8");
+    const rawData = readFileSync(path.join(__dirname, "temp_data", "todayData.json"), "utf-8");
     const petData = JSON.parse(rawData); // convert to JS object
     res.json(petData);
   } catch (error) {
