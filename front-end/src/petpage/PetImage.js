bd7618dd49a8915bea5880c3e1d6f97fc1c990b6
Sanna Moon <sm10454@nyu.edu>
2025-12-02 16:19:27 -0500

    1

--- front-end/src/petpage/PetImage.js
+++ front-end/src/petpage/PetImage.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import './PetPage.css'
+import React from "react";
 
-import stage1Img from 'front-end/src/petpage/pet_stage1.jpg';
-import stage2Img from 'front-end/src/petpage/pet_stage2.jpg';
-import stage2Img from 'front-end/src/petpage/pet_stage3.jpg';
+import stage1Img from "./pet_stage1.jpg";
+import stage2Img from "./pet_stage2.jpg";
+import stage3Img from "./pet_stage3.jpg";
 
 const stageImages = {
-    1: stage1Img,
-    2: stage2Img,
-    3: stage3Img,
-  };
+  1: stage1Img,
+  2: stage2Img,
+  3: stage3Img,
+};
 
 export default function PetImage({ stage }) {
-    return (
-      <img
-        src={stageImages[stage] || stageImages[1]}
-        alt={`Pet stage ${stage}`}
-        className="pet-image"
-      />
-    );
-  }
+  return (
+    <img
+      src={stageImages[stage] || stage1Img}
+      alt={`Pet stage ${stage}`}
+      style={{ width: "200px" }} 
+    />
+  );
+}
