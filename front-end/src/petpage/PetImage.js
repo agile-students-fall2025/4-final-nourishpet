a80ce46ade05c7b90918c9d2fe855907c0e9cceb
Sanna Moon <sm10454@nyu.edu>
2025-12-02 16:12:38 -0500

    insert images

--- front-end/src/petpage/PetImage.js
+++ front-end/src/petpage/PetImage.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './PetPage.css'
 
-function PetImage({petName}){
-    return( 
-        <div className='pet-image'>
-            <p>{petName}</p>
-            <img
-                src='https://picsum.photos/200/300'
-                alt='Pet'
-                className="pet-photo"
-            />
-        </div>
-    );
-}
+import stage1Img from 'front-end/src/petpage/pet_stage1.jpg';
+import stage2Img from 'front-end/src/petpage/pet_stage2.jpg';
+import stage2Img from 'front-end/src/petpage/pet_stage3.jpg';
 
-export default PetImage;
+const stageImages = {
+    1: stage1Img,
+    2: stage2Img,
+    3: stage3Img,
+  };
+
+export default function PetImage({ stage }) {
+    return (
+      <img
+        src={stageImages[stage] || stageImages[1]}
+        alt={`Pet stage ${stage}`}
+        className="pet-image"
+      />
+    );
+  }
